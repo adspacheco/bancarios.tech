2aa9ad8a364827d3994fd6cb7f9cfbb696c65fd4
adspacheco <andersonac7@outlook.com>
2026-02-08 17:43:02 -0300

    make `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -14,13 +14,14 @@ async function query(queryInput) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryInput);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
